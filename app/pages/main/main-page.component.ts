2fcf00094b76db08156b4155e2e9125a843db22f
bfv <dev@bfv.io>
2017-10-13 11:38:39 +0200

    added main page

---
+++ app/pages/main/main-page.component.ts
@@ -0,0 +1,15 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'main-page',
+    moduleId: module.id,
+    templateUrl: 'main-page.component.html',
+    styleUrls: ['main-page.component.css'],
+})
+
+export class MainPageComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
