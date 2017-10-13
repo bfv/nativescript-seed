e6ddeccddc70f111e11882894d8c0c409801591d
bfv <dev@bfv.io>
2017-10-13 11:39:57 +0200

    delete Item*, replaced by main page

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { Routes } from "@angular/router";
 
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { MainPageComponent } from './pages/main/main-page.component';
 
 const routes: Routes = [
-    { path: "", redirectTo: "/items", pathMatch: "full" },
-    { path: "items", component: ItemsComponent },
-    { path: "item/:id", component: ItemDetailComponent },
+    { path: "", redirectTo: "/main", pathMatch: "full" },
+    { path: "main", component: MainPageComponent },
 ];
 
 @NgModule({
--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,7 @@ import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
-import { ItemService } from "./item/item.service";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
-
+import { MainPageComponent } from './pages/main/main-page.component';
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
@@ -23,11 +20,10 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
+        MainPageComponent,
     ],
     providers: [
-        ItemService
+
     ],
     schemas: [
         NO_ERRORS_SCHEMA
